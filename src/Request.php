df09ffca1671ed0927c2a6aa6688bcacfecaf61d
lin <465382251@qq.com>
2020-09-22 11:10:55 +0800

    add headers

--- src/Request.php
+++ src/Request.php
@@ -88,11 +88,9 @@ class Request
      * 请求设置
      * */
     protected function options(){
-        $this->options=array_merge([
-            'headers'=>$this->headers,
-            //'verify'=>false
-        ],$this->options);
+        if(isset($this->options['headers'])) $this->headers=array_merge($this->headers,$this->options['headers']);
 
+        $this->options['headers']=$this->headers;
         $this->options['timeout'] = $this->options['timeout'] ?? 60;
 
         if(isset($this->options['proxy']) && $this->options['proxy']===true) {
